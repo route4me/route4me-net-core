74cd06777427e88b395c6225f2fd69177ed90daa
olman62 <oleg.guchi@gmail.com>
2020-11-04 20:44:30 +0400

    update c# SDK (.net core)
    Route address examples

--- route4me-csharp-sdk/Route4MeSDKTest/Examples/Addresses/AddDestinationToOptimization.cs
+++ route4me-csharp-sdk/Route4MeSDKTest/Examples/Addresses/AddDestinationToOptimization.cs
@@ -1,18 +1,25 @@
-﻿using Route4MeSDK.DataTypes;
+﻿using System.Collections.Generic;
+using Route4MeSDK.DataTypes;
 using Route4MeSDK.QueryTypes;
-using System;
 
 namespace Route4MeSDK.Examples
 {
     public sealed partial class Route4MeExamples
     {
-        public DataObject AddDestinationToOptimization(string optimizationProblemID, bool andReOptimize)
+        /// <summary>
+        /// Add a destination to an optimization.
+        /// </summary>
+        public void AddDestinationToOptimization()
         {
             // Create the manager with the api key
             var route4Me = new Route4MeManager(ActualApiKey);
 
+            RunOptimizationSingleDriverRoute10Stops();
+
+            OptimizationsToRemove = new List<string>() { SD10Stops_optimization_problem_id };
+
             // Prepare the address that we are going to add to an existing route optimization
-            var addresses = new Address[]
+            Address[] addresses = new Address[]
             {
                 new Address() { AddressString = "717 5th Ave New York, NY 10021",
                                 Alias         = "Giorgio Armani",
@@ -23,31 +30,21 @@ namespace Route4MeSDK.Examples
             };
 
             //Optionally change any route parameters, such as maximum route duration, maximum cubic constraints, etc.
-            var optimizationParameters = new OptimizationParameters()
+            OptimizationParameters optimizationParameters = new OptimizationParameters()
             {
-                OptimizationProblemID = optimizationProblemID,
+                OptimizationProblemID = SD10Stops_optimization_problem_id,
                 Addresses = addresses,
-                ReOptimize = andReOptimize
+                ReOptimize = true
             };
 
             // Execute the optimization to re-optimize and rebalance all the routes in this optimization
-            DataObject dataObject = route4Me.UpdateOptimization(optimizationParameters, out string errorString);
+            DataObject dataObject = route4Me.UpdateOptimization(
+                optimizationParameters,
+                out string errorString);
 
-            Console.WriteLine("");
+            PrintExampleOptimizationResult("AddDestinationToOptimization", dataObject, errorString);
 
-            if (dataObject != null)
-            {
-                Console.WriteLine("AddDestinationToOptimization executed successfully");
-
-                Console.WriteLine("Optimization Problem ID: {0}", dataObject.OptimizationProblemId);
-                Console.WriteLine("State: {0}", dataObject.State);
-            }
-            else
-            {
-                Console.WriteLine("AddDestinationToOptimization error: {0}", errorString);
-            }
-
-            return dataObject;
+            RemoveTestOptimizations();
         }
     }
 }
--- route4me-csharp-sdk/Route4MeSDKTest/Examples/Addresses/MoveDestinationToRoute.cs
+++ route4me-csharp-sdk/Route4MeSDKTest/Examples/Addresses/MoveDestinationToRoute.cs
@@ -1,17 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Route4MeSDK.Examples
 {
     public sealed partial class Route4MeExamples
     {
-        public void MoveDestinationToRoute(string toRouteId, int routeDestinationId, int afterDestinationId)
+        /// <summary>
+        /// Move a destination to a route after specified destination.
+        /// </summary>
+        /// <param name="toRouteId">Route ID of a destination route</param>
+        /// <param name="routeDestinationId">Source destination ID</param>
+        /// <param name="afterDestinationId">A destination id in a destination route. </param>
+        public void MoveDestinationToRoute(
+            string toRouteId = null,
+            int? routeDestinationId = null,
+            int? afterDestinationId = null)
         {
             // Create the manager with the api key
-            Route4MeManager route4Me = new Route4MeManager(ActualApiKey);
+            var route4Me = new Route4MeManager(ActualApiKey);
+
+            bool isInnerExample = toRouteId == null ? true : false;
+
+            if (isInnerExample)
+            {
+                RunOptimizationSingleDriverRoute10Stops();
+
+                routeDestinationId = (int)SD10Stops_route.Addresses[2].RouteDestinationId;
+
+                RunSingleDriverRoundTrip();
+
+                OptimizationsToRemove = new List<string>()
+                {
+                    SD10Stops_optimization_problem_id,
+                    SDRT_optimization_problem_id
+                };
+
+                toRouteId = SDRT_route_id;
+
+                if (toRouteId == null || toRouteId.Length != 32)
+                {
+                    Console.WriteLine("Cannot get a route to move the destination");
+                    RemoveTestOptimizations();
+                    return;
+                }
+
+                afterDestinationId = (int)SDRT_route.Addresses[3].RouteDestinationId;
+            }
 
             // Run the query
-            string errorString;
-            bool success = route4Me.MoveDestinationToRoute(toRouteId, routeDestinationId, afterDestinationId, out errorString);
+            bool success = route4Me.MoveDestinationToRoute(
+                toRouteId,
+                (int)routeDestinationId,
+                (int)afterDestinationId,
+                out string errorString);
 
             Console.WriteLine("");
 
@@ -26,6 +67,7 @@ namespace Route4MeSDK.Examples
                 Console.WriteLine("MoveDestinationToRoute error: {0}", errorString);
             }
 
+            if (isInnerExample) RemoveTestOptimizations();
         }
     }
 }
