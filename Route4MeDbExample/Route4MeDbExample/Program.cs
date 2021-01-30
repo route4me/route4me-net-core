2117f4e8a088148cf56fe8ac54579b7707e50928
olman62 <oleg.guchi@gmail.com>
2021-01-30 16:30:38 +0400

    update Route4MeDbExample project

--- Route4MeDbExample/Route4MeDbExample/Program.cs
+++ Route4MeDbExample/Route4MeDbExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Route4MeDB;
 
 namespace Route4MeDbExample
 {
@@ -11,20 +12,20 @@ namespace Route4MeDbExample
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            var runExample = new RunExamples(Route4MeDB.Route4MeDbLibrary.DatabaseProviders.InMemory);
+            var runExample = new RunExamples(Route4MeDB.Route4MeDbLibrary.DatabaseProviders.SQLite);
 
             runExample.c_ApiKey = "11111111111111111111111111111111";
 
             #region // Run the examples. Note: uncomment a line for executing.
             // runExample.CreateRoute4MeDatabase();
 
-            // runExample.CreateAddressBookContact();
+            runExample.CreateAddressBookContact();
 
             // runExample.CopyRouteJsonResponseToDatabase();
 
             // runExample.CreateOptimizationAndSaveToDatabase();
 
-            runExample.ExportOrderEntityToSdkOrderObject();
+            //runExample.ExportOrderEntityToSdkOrderObject();
             #endregion
 
             Console.ReadKey();
