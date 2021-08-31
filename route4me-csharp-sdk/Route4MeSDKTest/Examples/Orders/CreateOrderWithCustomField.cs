024bb05bda8d519dc240b3a83984196cba10e90c
Amazos <oleg.guchi@gmail.com>
2021-08-31 20:03:42 +0400

    update c# SDK (.net core 3.1)

--- route4me-csharp-sdk/Route4MeSDKTest/Examples/Orders/CreateOrderWithCustomField.cs
+++ route4me-csharp-sdk/Route4MeSDKTest/Examples/Orders/CreateOrderWithCustomField.cs
@@ -17,7 +17,7 @@ namespace Route4MeSDK.Examples
                 Address1 = "1358 E Luzerne St, Philadelphia, PA 19124, US",
                 CachedLat = 48.335991,
                 CachedLng = 31.18287,
-                DayScheduledFor_YYMMDD = "2019-10-11",
+                DayScheduledFor_YYYYMMDD = "2019-10-11",
                 AddressAlias = "Auto test address",
                 CustomUserFields = new OrderCustomField[]
                 {
