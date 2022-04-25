ff21b751881fd89476e311e4bcf1d452aa93f46d
Amazos <oleg.guchi@gmail.com>
2022-04-25 15:54:53 +0400

    fix: fixed data type classes and library core files
    Signed-off-by: Dmitry Koval <Dmitry_Koval@epam.com>

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/TimePredictionModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace Route4MeSDK.DataTypes
+{
+    /// <summary>
+    /// Time prediction model
+    /// </summary>
+    [DataContract]
+    public sealed class TimePredictionModel
+    {
+        /// <summary>
+        /// An action model (e.g. 'matrix', 'optimization', 'direction')
+        /// </summary>
+        [DataMember(Name = "model", EmitDefaultValue = false)]
+        public string Model { get; set; }
+
+        /// <summary>
+        /// Time-consuming prediction for an action model.
+        /// </summary>
+        [DataMember(Name = "value", EmitDefaultValue = false)]
+        public long Value { get; set; }
+
+        /// <summary>
+        /// Time unit (e.g. 'seconds', 'minutes')
+        /// </summary>
+        [DataMember(Name = "unit", EmitDefaultValue = false)]
+        public string Unit { get; set; }
+    }
+}
