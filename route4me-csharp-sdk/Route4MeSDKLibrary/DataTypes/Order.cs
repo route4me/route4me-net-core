37e0566dbabea280ec1fbc034a10268c005a7783
Amazos <oleg.guchi@gmail.com>
2022-04-25 15:54:53 +0400

    fix: fixed data type classes and library core files

--- route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/Order.cs
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/Order.cs
@@ -11,10 +11,46 @@ namespace Route4MeSDK.DataTypes
     public sealed class Order : GenericParameters
     {
         /// <summary>
+        /// Timestamp of an order creation.
+        /// </summary>
+        [DataMember(Name = "created_timestamp", EmitDefaultValue = false)]
+        public long? CreatedTimestamp { get; set; }
+
+
+        /// <summary>
+        /// Timestamp of an order creation.
+        /// </summary>
+        [DataMember(Name = "updated_timestamp", EmitDefaultValue = false)]
+        public long? UpdatedTimestamp { get; set; }
+
+
+        /// <summary>
         ///     Order ID
         /// </summary>
         [DataMember(Name = "order_id", EmitDefaultValue = false)]
         public long? OrderId { get; set; }
+
+        /// <summary>
+        ///     Order status ID. Available values: 
+        ///     0: New, 
+        ///     1: Inbound Scan, 
+        ///     2: Sorted by Territory, 
+        ///     3: Loaded, 
+        ///     4: Missing, 
+        ///     5: Damaged, 
+        ///     6: Manually Loaded, 
+        ///     7: Routed, 
+        ///     8: Unrouted, 
+        ///     9: Sorted by Route, 
+        ///     10: Route Started, 
+        ///     11: Failed, 
+        ///     12: Skipped, 
+        ///     13: Done, 
+        ///     14: Cancelled, 
+        ///     15: Scheduled
+        /// </summary>
+        [DataMember(Name = "order_status_id", EmitDefaultValue = false)]
+        public int OrderStatusId { get; set; }
 
         /// <summary>
         ///     Address 1 field. Required
@@ -29,6 +65,12 @@ namespace Route4MeSDK.DataTypes
         public string Address2 { get; set; }
 
         /// <summary>
+        ///     The id of the member inside the route4me system
+        /// </summary>
+        [DataMember(Name = "member_id", EmitDefaultValue = false)]
+        public long MemberId { get; set; }
+
+        /// <summary>
         ///     Geo latitude. Required
         /// </summary>
         [DataMember(Name = "cached_lat")]
@@ -41,6 +83,12 @@ namespace Route4MeSDK.DataTypes
         public double CachedLng { get; set; }
 
         /// <summary>
+        /// Order icon hexadecimal color on a map.
+        /// </summary>
+        [DataMember(Name = "color", EmitDefaultValue = false)]
+        public string Color { get; set; }
+
+        /// <summary>
         ///     Generate optimal routes and driving directions to this curbside latitude
         /// </summary>
         [DataMember(Name = "curbside_lat", EmitDefaultValue = false)]
@@ -51,6 +99,30 @@ namespace Route4MeSDK.DataTypes
         /// </summary>
         [DataMember(Name = "curbside_lng", EmitDefaultValue = false)]
         public double? CurbsideLng { get; set; }
+
+        /// <summary>
+        /// In how many routes is included address.
+        /// </summary>
+        [DataMember(Name = "in_route_count", EmitDefaultValue = false)]
+        public int? InRouteCount { get; set; }
+
+        /// <summary>
+        /// When the last time an address was visited.
+        /// </summary>
+        [DataMember(Name = "last_visited_timestamp", EmitDefaultValue = false)]
+        public long? LastVisitedTimestamp { get; set; }
+
+        /// <summary>
+        /// When last time an address was included in a route.
+        /// </summary>
+        [DataMember(Name = "last_routed_timestamp", EmitDefaultValue = false)]
+        public long? LastRoutedTimestamp { get; set; }
+
+        /// <summary>
+        /// Order inserted date
+        /// </summary>
+        [DataMember(Name = "day_added_YYMMDD")]
+        public string DayAddedYYMMDD { get; set; }
 
         /// <summary>
         ///     Scheduled day (format: yyyy-MM-dd)
@@ -95,40 +167,10 @@ namespace Route4MeSDK.DataTypes
         public long? ServiceTime { get; set; }
 
         /// <summary>
-        ///     Address City
+        ///     Local timezone string
         /// </summary>
-        [DataMember(Name = "address_city", EmitDefaultValue = false)]
-        public string AddressCity { get; set; }
-
-        /// <summary>
-        ///     Address state ID
-        /// </summary>
-        [DataMember(Name = "address_state_id", EmitDefaultValue = false)]
-        public string AddressStateId { get; set; }
-
-        /// <summary>
-        ///     Address country ID
-        /// </summary>
-        [DataMember(Name = "address_country_id", EmitDefaultValue = false)]
-        public string AddressCountryId { get; set; }
-
-        /// <summary>
-        ///     Address ZIP code
-        /// </summary>
-        [DataMember(Name = "address_zip", EmitDefaultValue = false)]
-        public string AddressZip { get; set; }
-
-        /// <summary>
-        ///     Order status ID
-        /// </summary>
-        [DataMember(Name = "order_status_id", EmitDefaultValue = false)]
-        public int OrderStatusId { get; set; }
-
-        /// <summary>
-        ///     The id of the member inside the route4me system
-        /// </summary>
-        [DataMember(Name = "member_id", EmitDefaultValue = false)]
-        public long MemberId { get; set; }
+        [DataMember(Name = "local_timezone_string", EmitDefaultValue = false)]
+        public string LocalTimezoneString { get; set; }
 
         /// <summary>
         ///     First name
@@ -161,16 +203,94 @@ namespace Route4MeSDK.DataTypes
         public Dictionary<string, string> ExtFieldCustomData { get; set; }
 
         /// <summary>
-        ///     Local timezone string
+        ///     The weight of the order's cargo (2 digits precision).
         /// </summary>
-        [DataMember(Name = "local_timezone_string", EmitDefaultValue = false)]
-        public string LocalTimezoneString { get; set; }
+        [DataMember(Name = "EXT_FIELD_weight", EmitDefaultValue = false)]
+        public double? Weight { get; set; }
 
         /// <summary>
-        ///     Order icon
+        ///     Order cost (2 digits precision).
+        /// </summary>
+        [DataMember(Name = "EXT_FIELD_cost", EmitDefaultValue = false)]
+        public double? Cost { get; set; }
+
+        /// <summary>
+        ///     Order revenue (2 digits precision).
+        /// </summary>
+        [DataMember(Name = "EXT_FIELD_revenue", EmitDefaultValue = false)]
+        public double? Revenue { get; set; }
+
+        /// <summary>
+        ///     The cubic volume that this order consumes/contains on a vehicle (2 digits precision).
+        /// </summary>
+        [DataMember(Name = "EXT_FIELD_cube", EmitDefaultValue = false)]
+        public double? Cube { get; set; }
+
+        /// <summary>
+        ///     The number of pieces/palllets that this order consumes/contains on a vehicle (2 digits precision).
+        /// </summary>
+        [DataMember(Name = "EXT_FIELD_pieces", EmitDefaultValue = false)]
+        public double? Pieces { get; set; }
+
+        /// <summary>
+        ///     The city the address is located in
+        /// </summary>
+        [DataMember(Name = "address_city", EmitDefaultValue = false)]
+        public string AddressCity { get; set; }
+
+        /// <summary>
+        ///     The state the address is located in
+        /// </summary>
+        [DataMember(Name = "address_state_id", EmitDefaultValue = false)]
+        public string AddressStateId { get; set; }
+
+        /// <summary>
+        ///     The country the address is located in
+        /// </summary>
+        [DataMember(Name = "address_country_id", EmitDefaultValue = false)]
+        public string AddressCountryId { get; set; }
+
+        /// <summary>
+        ///     The zip code the address is located in
+        /// </summary>
+        [DataMember(Name = "address_zip", EmitDefaultValue = false)]
+        public string AddressZip { get; set; }
+
+        /// <summary>
+        ///     URL to an order icon file
         /// </summary>
         [DataMember(Name = "order_icon", EmitDefaultValue = false)]
         public string OrderIcon { get; set; }
+
+        /// <summary>
+        ///     If true, the order is validated.
+        /// </summary>
+        [DataMember(Name = "is_validated", EmitDefaultValue = false)]
+        public bool? IsValidated { get; set; }
+
+        /// <summary>
+        ///     If true, the order is pending.
+        /// </summary>
+        [DataMember(Name = "is_pending", EmitDefaultValue = false)]
+        public bool? IsPending { get; set; }
+
+        /// <summary>
+        ///     If true, the order is accepted.
+        /// </summary>
+        [DataMember(Name = "is_accepted", EmitDefaultValue = false)]
+        public bool? IsAccepted { get; set; }
+
+        /// <summary>
+        ///     If true, the order is started.
+        /// </summary>
+        [DataMember(Name = "is_started", EmitDefaultValue = false)]
+        public bool? IsStarted { get; set; }
+
+        /// <summary>
+        ///     If true, the order is completed.
+        /// </summary>
+        [DataMember(Name = "is_completed", EmitDefaultValue = false)]
+        public bool? IsCompleted { get; set; }
 
         /// <summary>
         ///     Custom user fields.
@@ -186,9 +306,22 @@ namespace Route4MeSDK.DataTypes
 
         /// <summary>
         ///     Route address stop type. For available values see Enums.AddressStopType
+        ///     DELIVERY, PICKUP, BREAK, MEETUP, SERVICE, VISIT, DRIVEBY
         /// </summary>
         [DataMember(Name = "address_stop_type")]
         public string AddressStopType { get; set; }
+
+        /// <summary>
+        /// Last status of the order.
+        /// </summary>
+        [DataMember(Name = "last_status")]
+        public string lastStatus { get; set; }
+
+        /// <summary>
+        /// An order sorted on date.
+        /// </summary>
+        [DataMember(Name = "sorted_on_date")]
+        public string SortedOnDate { get; set; }
 
         /// <summary>
         ///     System-wide unique code, which permits end-users (recipients)
@@ -197,5 +330,23 @@ namespace Route4MeSDK.DataTypes
         [DataMember(Name = "tracking_number", EmitDefaultValue = false)]
         [ReadOnly(true)]
         public string TrackingNumber { get; set; }
+
+        /// <summary>
+        ///     Root member ID
+        /// </summary>
+        [DataMember(Name = "root_member_id", EmitDefaultValue = false)]
+        public long? RootMemberId { get; set; }
+
+        /// <summary>
+        ///     A unique ID of a group the organizations or partners.
+        /// </summary>
+        [DataMember(Name = "organization_id", EmitDefaultValue = false)]
+        public int? OrganizationId { get; set; }
+
+        /// <summary>
+        ///     A day ID, when the order was completed.
+        /// </summary>
+        [DataMember(Name = "done_day_id", EmitDefaultValue = false)]
+        public int? DoneDayId { get; set; }
     }
 }
