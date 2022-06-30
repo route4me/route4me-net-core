87cd398aa725a54f3f4f238e78b1cf9e5cf60b4a
Dmitry Koval <Dmitry_Koval@epam.com>
2022-06-30 09:56:33 +0300

    Switch from SocketIoClientDotNet.core_2.2 (binaries) to SocketIoClientDotNet.Standard (nuget package, netstandard).

--- route4me-csharp-sdk/Route4MeSDKLibrary/FastProcessing/Options.cs
+++ route4me-csharp-sdk/Route4MeSDKLibrary/FastProcessing/Options.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Quobject.SocketIoClientDotNet.EngineIoClientDotNet.Client;
-using Quobject.SocketIoClientDotNet.Client;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
+using Quobject.EngineIoClientDotNet.Client;
 
 namespace Route4MeSDK.FastProcessing
 {
