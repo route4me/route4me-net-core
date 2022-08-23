1c0e87097e9a8b7107eee00ad018d96f45a4d81a
Dmitry Koval <Dmitry_Koval@epam.com>
2022-08-23 11:04:13 +0300

    Route Status API supported

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/RouteStatus/UpdateRouteStatusAsPlannedResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.RouteStatus
+{
+    /// <summary>
+    ///     The response for route status request
+    /// </summary>
+    [DataContract]
+    public class UpdateRouteStatusAsPlannedResponse
+    {
+        /// <summary>
+        ///     Route status data.
+        /// </summary>
+        [DataMember(Name = "data", EmitDefaultValue = false)]
+        public RouteStatusData[] Data { get; set; }
+    }
+}
--- route4me-csharp-sdk/Route4MeSDKLibrary/Route4MeManagerV5.cs
+++ route4me-csharp-sdk/Route4MeSDKLibrary/Route4MeManagerV5.cs
@@ -19,6 +19,7 @@ using Route4MeSDKLibrary.DataTypes.V5;
 using Route4MeSDKLibrary.DataTypes.V5.AddressBookContact;
 using Route4MeSDKLibrary.DataTypes.V5.Internal.Requests;
 using Route4MeSDKLibrary.DataTypes.V5.Orders;
+using Route4MeSDKLibrary.DataTypes.V5.RouteStatus;
 using Route4MeSDKLibrary.QueryTypes.V5.Orders;
 using AddressBookParameters = Route4MeSDK.QueryTypes.V5.AddressBookParameters;
 using OptimizationParameters = Route4MeSDK.QueryTypes.V5.OptimizationParameters;
@@ -3899,6 +3900,184 @@ namespace Route4MeSDK
             return Task.FromResult(
                 new Tuple<OrderHistoryResponse, ResultResponse>(result.Result.Item1, result.Result.Item2)
                 );
+        }
+
+        #endregion
+
+        #region Route Status
+
+        /// <summary>
+        /// Get the status by specifying the path parameter ID.
+        /// </summary>
+        /// <param name="routeId">ID of the route</param>
+        /// <param name="resultResponse">Error response</param>
+        /// <returns></returns>
+        public RouteStatusResponse GetRouteStatus(string routeId, out ResultResponse resultResponse)
+        {
+            var url = R4MEInfrastructureSettingsV5.RouteStatus.Replace("{routeId}", routeId);
+
+            return GetJsonObjectFromAPI<RouteStatusResponse>(new GenericParameters(),
+                url,
+                HttpMethodType.Get,
+                out resultResponse);
+        }
+
+        /// <summary>
+        /// Get the status by specifying the path parameter ID.
+        /// </summary>
+        /// <param name="routeId">ID of the route</param>
+        /// <returns></returns>
+        public Task<Tuple<RouteStatusResponse, ResultResponse>> GetRouteStatusAsync(string routeId)
+        {
+            var url = R4MEInfrastructureSettingsV5.RouteStatus.Replace("{routeId}", routeId);
+
+            return GetJsonObjectFromAPIAsync<RouteStatusResponse>(new GenericParameters(),
+                url,
+                HttpMethodType.Get);
+        }
+
+        /// <summary>
+        /// Update the status by specifying the path parameter ID.Route statuses change only forward - planned > started/paused > completed
+        /// </summary>
+        /// <param name="routeId">ID of the route</param>
+        /// <param name="request">Request body</param>
+        /// <param name="resultResponse">Error response</param>
+        /// <returns></returns>
+        public RouteStatusResponse UpdateRouteStatus(string routeId, RouteStatusRequest request, out ResultResponse resultResponse)
+        {
+            var url = R4MEInfrastructureSettingsV5.RouteStatus.Replace("{routeId}", routeId);
+
+            return GetJsonObjectFromAPI<RouteStatusResponse>(request,
+                url,
+                HttpMethodType.Post,
+                out resultResponse);
+        }
+
+        /// <summary>
+        /// Update the status by specifying the path parameter ID.Route statuses change only forward - planned > started/paused > completed
+        /// </summary>
+        /// <param name="routeId">ID of the route</param>
+        /// <param name="request">Request body</param>
+        /// <returns></returns>
+        public Task<Tuple<RouteStatusResponse, ResultResponse>> UpdateRouteStatusAsync(string routeId, RouteStatusRequest request)
+        {
+            var url = R4MEInfrastructureSettingsV5.RouteStatus.Replace("{routeId}", routeId);
+
+            return GetJsonObjectFromAPIAsync<RouteStatusResponse>(request,
+                url,
+                HttpMethodType.Post);
+        }
+
+        /// <summary>
+        /// Get route status history by specifying the path parameter ID.
+        /// </summary>
+        /// <param name="parameters">Query parameters</param>
+        /// <param name="resultResponse">Error response</param>
+        /// <returns></returns>
+        public RouteStatusHistoryResponse GetRouteStatusHistory(RouteStatusHistoryParameters parameters, out ResultResponse resultResponse)
+        {
+            var url = R4MEInfrastructureSettingsV5.RouteStatusHistory.Replace("{routeId}", parameters.RouteId);
+
+            return GetJsonObjectFromAPI<RouteStatusHistoryResponse>(parameters,
+                url,
+                HttpMethodType.Get,
+                out resultResponse);
+        }
+
+        /// <summary>
+        /// Get route status history by specifying the path parameter ID.
+        /// </summary>
+        /// <param name="parameters">Query parameters</param>
+        /// <returns></returns>
+        public Task <Tuple<RouteStatusHistoryResponse, ResultResponse>> GetRouteStatusHistoryAsync(RouteStatusHistoryParameters parameters)
+        {
+            var url = R4MEInfrastructureSettingsV5.RouteStatusHistory.Replace("{routeId}", parameters.RouteId);
+
+            return GetJsonObjectFromAPIAsync<RouteStatusHistoryResponse>(parameters,
+                url,
+                HttpMethodType.Get);
+        }
+
+        /// <summary>
+        /// Roll back route status by specifying the path parameter ID. Sometimes a status rollback is possible.
+        /// </summary>
+        /// <param name="routeId">Route ID</param>
+        /// <param name="resultResponse">Error response</param>
+        /// <returns></returns>
+        public RouteStatusResponse RollbackRouteStatus(string routeId, out ResultResponse resultResponse)
+        {
+            var url = R4MEInfrastructureSettingsV5.RollbackRouteStatus.Replace("{routeId}", routeId);
+
+            return GetJsonObjectFromAPI<RouteStatusResponse>(new GenericParameters(),
+                url,
+                HttpMethodType.Get,
+                out resultResponse);
+        }
+
+        /// <summary>
+        /// Roll back route status by specifying the path parameter ID. Sometimes a status rollback is possible.
+        /// </summary>
+        /// <param name="routeId">Route ID</param>
+        /// <returns></returns>
+        public Task<Tuple<RouteStatusResponse, ResultResponse>> RollbackRouteStatusAsync(string routeId)
+        {
+            var url = R4MEInfrastructureSettingsV5.RollbackRouteStatus.Replace("{routeId}", routeId);
+
+            return GetJsonObjectFromAPIAsync<RouteStatusResponse>(new GenericParameters(),
+                url,
+                HttpMethodType.Get);
+        }
+
+        /// <summary>
+        /// Store a new Status in the database with planned status.
+        /// </summary>
+        /// <param name="parameters">Route IDs</param>
+        /// <param name="resultResponse">Error response</param>
+        /// <returns></returns>
+        public UpdateRouteStatusAsPlannedResponse UpdateRouteStatusAsPlanned(UpdateRouteStatusAsPlannedParameters parameters, out ResultResponse resultResponse)
+        {
+            return GetJsonObjectFromAPI<UpdateRouteStatusAsPlannedResponse>(parameters,
+                R4MEInfrastructureSettingsV5.PlannedRouteStatus,
+                HttpMethodType.Post,
+                out resultResponse);
+        }
+
+        /// <summary>
+        /// Store a new Status in the database with planned status.
+        /// </summary>
+        /// <param name="parameters">Route IDs</param>
+        /// <returns></returns>
+        public Task<Tuple<UpdateRouteStatusAsPlannedResponse, ResultResponse>> UpdateRouteStatusAsPlanned(UpdateRouteStatusAsPlannedParameters parameters)
+        {
+            return GetJsonObjectFromAPIAsync<UpdateRouteStatusAsPlannedResponse>(parameters,
+                R4MEInfrastructureSettingsV5.PlannedRouteStatus,
+                HttpMethodType.Post);
+        }
+
+        /// <summary>
+        /// Store a new Status in the database with planned status.
+        /// </summary>
+        /// <param name="parameters">parameters</param>
+        /// <param name="resultResponse">Error response</param>
+        /// <returns></returns>
+        public StatusResponse SetRouteStopStatus(SetRouteStopStatusParameters parameters, out ResultResponse resultResponse)
+        {
+            return GetJsonObjectFromAPI<StatusResponse>(parameters,
+                R4MEInfrastructureSettingsV5.RouteStopStatus,
+                HttpMethodType.Post,
+                out resultResponse);
+        }
+
+        /// <summary>
+        /// Store a new Status in the database with planned status.
+        /// </summary>
+        /// <param name="parameters">Parameters</param>
+        /// <returns></returns>
+        public Task<Tuple<StatusResponse, ResultResponse>> SetRouteStopStatusAsync(SetRouteStopStatusParameters parameters)
+        {
+            return GetJsonObjectFromAPIAsync<StatusResponse>(parameters,
+                R4MEInfrastructureSettingsV5.RouteStopStatus,
+                HttpMethodType.Post);
         }
 
         #endregion
