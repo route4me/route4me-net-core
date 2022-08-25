d1026fe657d90632974c96ce9e08949b52ee269e
Dmitry Koval <Dmitry_Koval@epam.com>
2022-08-25 14:21:27 +0300

    GetServiceTimeByAddressType is introduced

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/ServiceTimeUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Route4MeSDKLibrary
+{
+    public static class ServiceTimeUtils
+    {
+        public static long? GetServiceTimeByAddressType(string key, Dictionary<string, int?> serviceTimeMap)
+        {
+            return serviceTimeMap.ContainsKey(key) ? serviceTimeMap[key] : (long?)null;
+        }
+    }
+}
