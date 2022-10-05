6f54d154a46330e2faf8a1c51c1058e102481e61
Dmitry Koval <Dmitry_Koval@epam.com>
2022-10-05 18:00:04 +0300

    Restored route query API.

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/Routes/RoutesResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using Route4MeSDK.DataTypes.V5;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.Routes
+{
+    [DataContract]
+    public class RoutesResponse
+    {
+        [DataMember(Name = "data", EmitDefaultValue = false)]
+        public DataObjectRoute[] Data { get; set; }
+    }
+}
