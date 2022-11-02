60c932d3bfb81dddbf024d1017921c51f3e5c4a4
Dmitry Koval <Dmitry_Koval@epam.com>
2022-11-02 20:08:06 +0300

    Split V5 manager by sub-managers. Fixed unit tests.

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/Managers/OptimizationManagerV5.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading.Tasks;
+using Route4MeSDK;
+using Route4MeSDK.DataTypes.V5;
+using Route4MeSDK.QueryTypes.V5;
+using Route4MeSDKLibrary.DataTypes.Internal.Requests;
+using Route4MeSDKLibrary.DataTypes.Internal.Response;
+
+namespace Route4MeSDKLibrary.Managers
+{
+    public class OptimizationManagerV5 : Route4MeManagerBase
+    {
+        public OptimizationManagerV5(string apiKey) : base(apiKey)
+        {
+        }
+
+        /// <summary>
+        ///     Generates optimized routes
+        /// </summary>
+        /// <param name="optimizationParameters">
+        ///     The input parameters for the routes optimization, which encapsulates:
+        ///     the route parameters and the addresses.
+        /// </param>
+        /// <param name="resultResponse">Failing response</param>
+        /// <returns>Generated optimization problem object</returns>
+        public DataObject RunOptimization(OptimizationParameters optimizationParameters,
+            out ResultResponse resultResponse)
+        {
+            var result = GetJsonObjectFromAPI<DataObject>(optimizationParameters,
+                R4MEInfrastructureSettings.ApiHost,
+                HttpMethodType.Post,
+                false,
+                true,
+                out resultResponse);
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Generates optimized routes
+        /// </summary>
+        /// <param name="optimizationParameters">
+        ///     The input parameters for the routes optimization, which encapsulates:
+        ///     the route parameters and the addresses.
+        /// </param>
+        /// <returns>Generated optimization problem object</returns>
+        public async Task<Tuple<DataObject, ResultResponse>> RunOptimizationAsync(OptimizationParameters optimizationParameters)
+        {
+            var result = await GetJsonObjectFromAPIAsync<DataObject>(optimizationParameters,
+                R4MEInfrastructureSettings.ApiHost,
+                HttpMethodType.Post,
+                null,
+                true,
+                false).ConfigureAwait(false);
+
+            return new Tuple<DataObject, ResultResponse>(result.Item1, result.Item2);
+        }
+
+        /// <summary>
+        ///     Remove an existing optimization belonging to an user.
+        /// </summary>
+        /// <param name="optimizationProblemIDs"> Optimization Problem IDs </param>
+        /// <param name="resultResponse"> Returned error string in case of the processs failing </param>
+        /// <returns> Result status true/false </returns>
+        public bool RemoveOptimization(string[] optimizationProblemIDs, out ResultResponse resultResponse)
+        {
+            var remParameters = new RemoveOptimizationRequest
+            {
+                Redirect = 0,
+                OptimizationProblemIds = optimizationProblemIDs
+            };
+
+            var response = GetJsonObjectFromAPI<RemoveOptimizationResponse>(remParameters,
+                R4MEInfrastructureSettings.ApiHost,
+                HttpMethodType.Delete,
+                out resultResponse);
+            if (response != null)
+            {
+                if (response.Status && response.Removed > 0) return true;
+                return false;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Remove an existing optimization belonging to an user.
+        /// </summary>
+        /// <param name="optimizationProblemIDs"> Optimization Problem IDs </param>
+        /// <returns> Result status true/false </returns>
+        public async Task<Tuple<bool, ResultResponse>> RemoveOptimizationAsync(string[] optimizationProblemIDs)
+        {
+            var remParameters = new RemoveOptimizationRequest
+            {
+                Redirect = 0,
+                OptimizationProblemIds = optimizationProblemIDs
+            };
+
+            var response = await GetJsonObjectFromAPIAsync<RemoveOptimizationResponse>(remParameters,
+                R4MEInfrastructureSettings.ApiHost,
+                HttpMethodType.Delete).ConfigureAwait(false);
+            if (response.Item1 != null)
+            {
+                if (response.Item1.Status && response.Item1.Removed > 0) return new Tuple<bool, ResultResponse>(true, response.Item2);
+                return new Tuple<bool, ResultResponse>(false, response.Item2);
+            }
+
+            return new Tuple<bool, ResultResponse>(false, response.Item2);
+        }
+    }
+}
--- route4me-csharp-sdk/Route4MeSdkV5UnitTest/V5/Vehicles/VehicleBulkOperationTests.cs
+++ route4me-csharp-sdk/Route4MeSdkV5UnitTest/V5/Vehicles/VehicleBulkOperationTests.cs
@@ -185,7 +185,7 @@ namespace Route4MeSdkV5UnitTest.V5.Vehicles
             var vehicles = route4me.ActivateVehicles(vehicleIDs, out ResultResponse resultResponse);
 
             Assert.NotNull(vehicles);
-            Assert.That(vehicles.GetType(), Is.EqualTo(typeof(Vehicle[])));
+            Assert.That(vehicles.GetType(), Is.EqualTo(typeof(VehiclesResults)));
         }
 
         [Test, Order(3)]
