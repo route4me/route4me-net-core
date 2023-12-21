72831c8c83e7e8ebbe4a29badebf3321d409fc0b
Dmitry Koval <Dmitry_Koval@epam.com>
2023-12-21 21:22:32 +0300

    Fixed OrdersHistoryTest/OrderHistoryAsyncTest

--- route4me-csharp-sdk/Route4MeSdkV5UnitTest/V5/Orders/OrdersTests.cs
+++ route4me-csharp-sdk/Route4MeSdkV5UnitTest/V5/Orders/OrdersTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Route4MeSDK;
+using Route4MeSDK.DataTypes;
 using Route4MeSDK.QueryTypes;
 using Route4MeSDK.QueryTypes.V5;
 using Route4MeSDKLibrary.QueryTypes.V5.Orders;
@@ -44,13 +46,20 @@ namespace Route4MeSdkV5UnitTest.V5.Orders
             var route4meV5 = new Route4MeManagerV5(ApiKeys.ActualApiKey);
             var route4me = new Route4MeManager(ApiKeys.ActualApiKey);
 
-            var orders = route4me.GetOrders(new OrderParameters() { Limit = 1 }, out _, out _);
-            Assert.True(orders.Length > 0);
+            var order = new Order()
+            {
+                Address1 = "Test Address1 " + (new Random()).Next().ToString(),
+                AddressAlias = "Test AddressAlias " + (new Random()).Next().ToString(),
+                CachedLat = 37.9,
+                CachedLng = -34.6
+            };
+
+            Order createdOrder = route4me.AddOrder(order, out string errorString);
 
             var parameters = new OrderHistoryParameters()
             {
-                OrderId = orders.Single().OrderId,
-                TrackingNumber = orders.Single().TrackingNumber
+                OrderId = createdOrder.OrderId,
+                TrackingNumber = createdOrder.TrackingNumber
             };
 
             var result = route4meV5.GetOrderHistory(parameters, out var resultResponse);
@@ -64,13 +73,20 @@ namespace Route4MeSdkV5UnitTest.V5.Orders
             var route4meV5 = new Route4MeManagerV5(ApiKeys.ActualApiKey);
             var route4me = new Route4MeManager(ApiKeys.ActualApiKey);
 
-            var orders = await route4me.GetOrdersAsync(new OrderParameters() { Limit = 1 });
-            Assert.True(orders.Item1.Length == 1);
+            var order = new Order()
+            {
+                Address1 = "Test Address1 " + (new Random()).Next().ToString(),
+                AddressAlias = "Test AddressAlias " + (new Random()).Next().ToString(),
+                CachedLat = 37.9,
+                CachedLng = -34.6
+            };
+
+            var createdOrder = await route4me.AddOrderAsync(order);
 
             var parameters = new OrderHistoryParameters()
             {
-                OrderId = orders.Item1.Single().OrderId,
-                TrackingNumber = orders.Item1.Single().TrackingNumber
+                OrderId = createdOrder.Item1.OrderId,
+                TrackingNumber = createdOrder.Item1.TrackingNumber
             };
 
             var result = await route4meV5.GetOrderHistoryAsync(parameters);
