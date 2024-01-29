967df2d2a2cd8c62a21022fea745a02d095fa8f7
pwnded <61501821+pwnded@users.noreply.github.com>
2024-01-29 21:11:00 +0300

    order_uuid is supported for Order V4. Order V5 API is supported (#14)
    * order_uuid is supported for Order V4.
    
    Order V5 API is supported.
    
    * Fixed mistyping.
    
    * Get rid of order_status_id for V5 Order.
    
    ---------
    
    Co-authored-by: Dmitry Koval <Dmitry_Koval@epam.com>

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/Orders/CustomUserFieldsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.Orders
+{
+    /// <summary>
+    /// Custom user fields response
+    /// </summary>
+    [DataContract]
+    public class CustomUserFieldsResponse
+    {
+        /// <summary>
+        ///     Data
+        /// </summary>
+        [DataMember(Name = "data", EmitDefaultValue = false)]
+        public OrderCustomField[] Data { get; set; }
+    }
+}
---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/Orders/SearchParamRequestBodyMatchTerms.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.Orders
+{
+    /// <summary>
+    /// Search Param Request Body Matches
+    /// </summary>
+    [DataContract]
+    public class SearchParamRequestBodyMatchTerms
+    {
+        /// <summary>
+        ///     Custom data
+        /// </summary>
+        [DataMember(Name = "custom_data", EmitDefaultValue = false)]
+        public JObject CustomData { get; set; }
+
+        /// <summary>
+        ///     First name
+        /// </summary>
+        [DataMember(Name = "first_name", EmitDefaultValue = false)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        ///     Last name
+        /// </summary>
+        [DataMember(Name = "last_name", EmitDefaultValue = false)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///     Email
+        /// </summary>
+        [DataMember(Name = "email", EmitDefaultValue = false)]
+        public string Email { get; set; }
+
+        /// <summary>
+        ///     Phone
+        /// </summary>
+        [DataMember(Name = "phone", EmitDefaultValue = false)]
+        public string Phone { get; set; }
+
+        /// <summary>
+        ///     Address 1
+        /// </summary>
+        [DataMember(Name = "address_1", EmitDefaultValue = false)]
+        public string Address1 { get; set; }
+
+        /// <summary>
+        ///     Address zip
+        /// </summary>
+        [DataMember(Name = "address_zip", EmitDefaultValue = false)]
+        public string AddressZip { get; set; }
+
+        /// <summary>
+        ///     Address alias
+        /// </summary>
+        [DataMember(Name = "address_alias", EmitDefaultValue = false)]
+        public string AddressAlias { get; set; }
+
+    }
+}
