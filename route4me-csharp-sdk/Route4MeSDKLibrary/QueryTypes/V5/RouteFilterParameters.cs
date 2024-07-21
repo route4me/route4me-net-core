af8b3a9ba0acfff82d534057416a8c774b959ea4
Dmitry Koval <Dmitry_Koval@epam.com>
2024-07-21 11:39:13 +0300

    Support all parameters for routes  filtering. Fixed optimization creation with profile id.

--- route4me-csharp-sdk/Route4MeSDKLibrary/QueryTypes/V5/RouteFilterParameters.cs
+++ route4me-csharp-sdk/Route4MeSDKLibrary/QueryTypes/V5/RouteFilterParameters.cs
@@ -12,11 +12,8 @@ namespace Route4MeSDK.QueryTypes.V5
         [DataMember(Name = "filters", EmitDefaultValue = false)]
         public RouteFilterParametersFilters Filters { get; set; }
 
-        //[DataMember(Name = "directions", EmitDefaultValue = false)]
-        //public bool? Directions { get; set; }
-
-        //[DataMember(Name = "notes", EmitDefaultValue = false)]
-        //public bool? Notes { get; set; }
+        [DataMember(Name = "search_query", EmitDefaultValue = false)]
+        public string SearchQuery { get; set; }
 
         [DataMember(Name = "page", EmitDefaultValue = false)]
         public int? Page { get; set; }
@@ -34,13 +31,178 @@ namespace Route4MeSDK.QueryTypes.V5
     [DataContract]
     public class RouteFilterParametersFilters : GenericParameters
     {
-        [DataMember(Name = "query", EmitDefaultValue = false)]
+        [DataMember(Name = "search_query", EmitDefaultValue = false)]
         public string Query { get; set; }
 
-        /// <summary>
-        ///     An array of the scheduled dates.
-        /// </summary>
+        [DataMember(Name = "route_id", EmitDefaultValue = false)]
+        public string RouteId { get; set; }
+
+        [DataMember(Name = "vehicle_id", EmitDefaultValue = false)]
+        public string VehicleId { get; set; }
+
+        [DataMember(Name = "member_id", EmitDefaultValue = false)]
+        public long? MemberId { get; set; }
+
+        [DataMember(Name = "optimization_problem_id", EmitDefaultValue = false)]
+        public string OptimizationProblemId { get; set; }
+
+        [DataMember(Name = "route_tx_source", EmitDefaultValue = false)]
+        public string RouteTxSource { get; set; }
+
         [DataMember(Name = "schedule_date", EmitDefaultValue = false)]
         public string[] ScheduleDate { get; set; }
+
+        [DataMember(Name = "created_date", EmitDefaultValue = false)]
+        public string[] CreatedDate { get; set; }
+
+        [DataMember(Name = "route_status", EmitDefaultValue = false)]
+        public string[] RouteStatus { get; set; }
+
+        [DataMember(Name = "contact_id", EmitDefaultValue = false)]
+        public long? ContactId { get; set; }
+
+        [DataMember(Name = "vehicle_ids", EmitDefaultValue = false)]
+        public string[] VehicleIds { get; set; }
+
+        [DataMember(Name = "member_ids", EmitDefaultValue = false)]
+        public long[] MemberIds { get; set; }
+
+        [DataMember(Name = "depot_address_id", EmitDefaultValue = false)]
+        public long? DepotAddressId { get; set; }
+
+        [DataMember(Name = "route_name", EmitDefaultValue = false)]
+        public string RouteName { get; set; }
+
+        [DataMember(Name = "created_timestamp", EmitDefaultValue = false)]
+        public long[] CreatedTimestamp { get; set; }
+
+        [DataMember(Name = "approved_to_execute", EmitDefaultValue = false)]
+        public bool? ApprovedToExecute { get; set; }
+
+        [DataMember(Name = "approved_to_execute_date", EmitDefaultValue = false)]
+        public long[] ApprovedToExecuteDate { get; set; }
+
+        [DataMember(Name = "destination_count", EmitDefaultValue = false)]
+        public int[] DestinationCount { get; set; }
+
+        [DataMember(Name = "visited_count", EmitDefaultValue = false)]
+        public int[] VisitedCount { get; set; }
+
+        [DataMember(Name = "notes_count", EmitDefaultValue = false)]
+        public int[] NotesCount { get; set; }
+
+        [DataMember(Name = "trip_distance", EmitDefaultValue = false)]
+        public double[] TripDistance { get; set; }
+
+        [DataMember(Name = "actual_travel_distance", EmitDefaultValue = false)]
+        public double[] ActualTravelDistance { get; set; }
+
+        [DataMember(Name = "actual_travel_time", EmitDefaultValue = false)]
+        public long[] ActualTravelTime { get; set; }
+
+        [DataMember(Name = "route_duration", EmitDefaultValue = false)]
+        public long[] RouteDuration { get; set; }
+
+        [DataMember(Name = "planned_total_route_duration", EmitDefaultValue = false)]
+        public long[] PlannedTotalRouteDuration { get; set; }
+
+        [DataMember(Name = "route_progress", EmitDefaultValue = false)]
+        public int[] RouteProgress { get; set; }
+
+        [DataMember(Name = "is_roundtrip", EmitDefaultValue = false)]
+        public bool? IsRoundtrip { get; set; }
+
+        [DataMember(Name = "smart_optimization_id", EmitDefaultValue = false)]
+        public string SmartOptimizationId { get; set; }
+
+        [DataMember(Name = "is_master", EmitDefaultValue = false)]
+        public bool IsMaster { get; set; }
+
+        [DataMember(Name = "is_last_locked", EmitDefaultValue = false)]
+        public bool? IsLastLocked { get; set; }
+
+        [DataMember(Name = "algorithm_type", EmitDefaultValue = false)]
+        public string AlgorithmType { get; set; }
+
+        [DataMember(Name = "organization_id", EmitDefaultValue = false)]
+        public string OrganizationId { get; set; }
+
+        [DataMember(Name = "master_route_id", EmitDefaultValue = false)]
+        public string MasterRouteId { get; set; }
+
+        [DataMember(Name = "route_start_time", EmitDefaultValue = false)]
+        public long[] RouteStartTime { get; set; }
+
+        [DataMember(Name = "route_end_time", EmitDefaultValue = false)]
+        public long[] RouteEndTime { get; set; }
+
+        [DataMember(Name = "original_route_id", EmitDefaultValue = false)]
+        public string OriginalRouteId { get; set; }
+
+        [DataMember(Name = "root_member_id", EmitDefaultValue = false)]
+        public long? RootMemberId { get; set; }
+
+        [DataMember(Name = "destinations_arrived_on_time", EmitDefaultValue = false)]
+        public int[] DestinationsArrivedOnTime { get; set; }
+
+        [DataMember(Name = "destinations_arrived_early", EmitDefaultValue = false)]
+        public int[] DestinationsArrivedEarly { get; set; }
+
+        [DataMember(Name = "destinations_arrived_late", EmitDefaultValue = false)]
+        public int[] DestinationsArrivedLate { get; set; }
+
+        [DataMember(Name = "actual_weight_load_percent", EmitDefaultValue = false)]
+        public int[] ActualWeightLoadPercent { get; set; }
+
+        [DataMember(Name = "planned_weight_load_percent", EmitDefaultValue = false)]
+        public int[] PlannedWeightLoadPercent { get; set; }
+
+        [DataMember(Name = "actual_cube_load_percent", EmitDefaultValue = false)]
+        public int[] ActualCubeLoadPercent { get; set; }
+
+        [DataMember(Name = "planned_cube_load_percent", EmitDefaultValue = false)]
+        public int[] PlannedCubeLoadPercent { get; set; }
+
+        [DataMember(Name = "actual_revenue_load_percent", EmitDefaultValue = false)]
+        public int[] ActualRevenueLoadPercent { get; set; }
+
+        [DataMember(Name = "planned_revenue_load_percent", EmitDefaultValue = false)]
+        public int[] PlannedRevenueLoadPercent { get; set; }
+
+        [DataMember(Name = "actual_pieces_load_percent", EmitDefaultValue = false)]
+        public int[] ActualPiecesLoadPercent { get; set; }
+
+        [DataMember(Name = "planned_pieces_load_percent", EmitDefaultValue = false)]
+        public int[] PlannedPiecesLoadPercent { get; set; }
+
+        [DataMember(Name = "mobile_actual_travel_distance", EmitDefaultValue = false)]
+        public double[] MobileActualTravelDistance { get; set; }
+
+        [DataMember(Name = "telematics_actual_travel_distance", EmitDefaultValue = false)]
+        public double[] TelematicsActualTravelDistance { get; set; }
+
+        [DataMember(Name = "failed_destinations", EmitDefaultValue = false)]
+        public int[] FailedDestinations { get; set; }
+
+        [DataMember(Name = "skipped_destinations", EmitDefaultValue = false)]
+        public int[] SkippedDestinations { get; set; }
+
+        [DataMember(Name = "done_destinations", EmitDefaultValue = false)]
+        public int[] DoneDestinations { get; set; }
+
+        [DataMember(Name = "planned_destinations", EmitDefaultValue = false)]
+        public int[] PlannedDestinations { get; set; }
+
+        [DataMember(Name = "cube", EmitDefaultValue = false)]
+        public int[] Cube { get; set; }
+
+        [DataMember(Name = "revenue", EmitDefaultValue = false)]
+        public int[] Revenue { get; set; }
+
+        [DataMember(Name = "pieces", EmitDefaultValue = false)]
+        public int[] Pieces { get; set; }
+
+        [DataMember(Name = "weight", EmitDefaultValue = false)]
+        public int[] Weight { get; set; }
     }
 }
---
+++ route4me-csharp-sdk/Route4MeSDKTest/Examples/API5/Routes/GetRoutesByRouteStatus.cs
@@ -0,0 +1,31 @@
+﻿using Route4MeSDK.DataTypes.V5;
+using Route4MeSDK.QueryTypes.V5;
+using Route4MeSDKLibrary.DataTypes.V5.RouteStatus;
+
+namespace Route4MeSDK.Examples
+{
+    public sealed partial class Route4MeExamples
+    {
+        // Retrieve the routes by timezone 
+        public void GetRoutesByRouteStatus()
+        {
+            var route4Me = new Route4MeManagerV5(ActualApiKey);
+
+            var routeParameters = new RouteFilterParameters
+            {
+                Page = 1,
+                PerPage = 20,
+                Filters = new RouteFilterParametersFilters()
+                {
+                    RouteStatus = new[] { RouteStatus.Planned.Description() }
+                }
+            };
+
+            var result = route4Me.GetRoutesByFilter(
+                routeParameters,
+                out ResultResponse resultResponse);
+
+            PrintExampleRouteResult(result, null, resultResponse);
+        }
+    }
+}
