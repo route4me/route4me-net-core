708991ce72701d6d3ad6b1c79e10d2e3f9145456
Dmitry Koval <Dmitry_Koval@epam.com>
2024-08-07 12:17:57 +0300

    optimization profile save/delete entities

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/OptimizationProfiles/OptimizationProfileDeleteEntitiesRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using Route4MeSDK.QueryTypes;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.OptimizationProfiles
+{
+    public class OptimizationProfileDeleteEntitiesRequest : GenericParameters
+    {
+        /// <summary>
+        ///     Items
+        /// </summary>
+        [DataMember(Name = "items", EmitDefaultValue = false)]
+        public OptimizationProfileDeleteEntitiesRequestItem[] Items { get; set; }
+    }
+
+    public class OptimizationProfileDeleteEntitiesRequestItem : GenericParameters
+    {
+        /// <summary>
+        ///     Id
+        /// </summary>
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public string Id { get; set; }
+    }
+}
--- route4me-csharp-sdk/Route4MeSdkV5UnitTest/V5/OptimizationProfiles/OptimizationProfileManagerV5Tests.cs
+++ route4me-csharp-sdk/Route4MeSdkV5UnitTest/V5/OptimizationProfiles/OptimizationProfileManagerV5Tests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 using Route4MeSDKLibrary.DataTypes.V5.OptimizationProfiles;
 using Route4MeSDKLibrary.Managers;
 
@@ -17,6 +19,71 @@ namespace Route4MeSdkV5UnitTest.V5.OptimizationProfiles
             var optimizationProfiles = route4Me.GetOptimizationProfiles(out _);
 
             Assert.That(optimizationProfiles.GetType(), Is.EqualTo(typeof(OptimizationProfilesResponse)));
+        }
+
+        [Test]
+        public void SaveEntitiesTest()
+        {
+            var route4Me = new OptimizationProfileManagerV5(CApiKey);
+
+            var result = route4Me.SaveEntities(
+                new OptimizationProfileSaveEntities()
+                {
+                    Items = new OptimizationProfileSaveEntitiesItem[1]
+                    {
+                        new()
+                        {
+                            Guid = "eaa", Id = "f09e3d22-c1d6-473c-8494-41563034b85b",
+                            Parts = new OptimizationProfileSaveEntitiesItemPart[1]
+                            {
+                                new()
+                                {
+                                    Guid = "pav",
+                                    Data = JObject.Parse("{\"append_date_to_route_name\":true}")
+                                }
+                            }
+                        }
+                    }
+                }, out var resultResponse);
+
+            Assert.That(result.GetType(), Is.EqualTo(typeof(OptimizationProfileSaveEntities)));
+        }
+
+        [Test]
+        public void DeleteEntitiesTest()
+        {
+            var route4Me = new OptimizationProfileManagerV5(CApiKey);
+
+            var saveResult = route4Me.SaveEntities(
+                new OptimizationProfileSaveEntities()
+                {
+                    Items = new OptimizationProfileSaveEntitiesItem[1]
+                    {
+                        new()
+                        {
+                            Guid = "eaa", Id = "f09e3d22-c1d6-473c-8494-41563034b85b",
+                            Parts = new OptimizationProfileSaveEntitiesItemPart[1]
+                            {
+                                new()
+                                {
+                                    Guid = "pav",
+                                    Data = JObject.Parse("{\"append_date_to_route_name\":true}")
+                                }
+                            }
+                        }
+                    }
+                }, out var resultResponse);
+
+
+            var result = route4Me.DeleteEntities(new OptimizationProfileDeleteEntitiesRequest()
+            {
+                Items = new OptimizationProfileDeleteEntitiesRequestItem[1]
+                {
+                    new OptimizationProfileDeleteEntitiesRequestItem() { Id = saveResult.Items.First().Id }
+                }
+            }, out resultResponse);
+
+            Assert.That(result.GetType(), Is.EqualTo(typeof(OptimizationProfileSaveEntities)));
         }
     }
 }
