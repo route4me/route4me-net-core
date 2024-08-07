7cad5d2f20df2d1cac0f0ab3a8dc9d85a8633dc7
Dmitry Koval <Dmitry_Koval@epam.com>
2024-08-07 12:17:57 +0300

    optimization profile save/delete entities

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/OptimizationProfiles/OptimizationProfileSaveEntities.cs
@@ -0,0 +1,57 @@
+﻿using Route4MeSDK.QueryTypes;
+using System.Runtime.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.OptimizationProfiles
+{
+    public class OptimizationProfileSaveEntities : GenericParameters
+    {
+        /// <summary>
+        ///     items
+        /// </summary>
+        [DataMember(Name = "items", EmitDefaultValue = false)]
+        public OptimizationProfileSaveEntitiesItem[] Items { get; set; }
+    }
+
+    public class OptimizationProfileSaveEntitiesItem : GenericParameters
+    {
+        /// <summary>
+        ///     Guid
+        /// </summary>
+        [DataMember(Name = "guid", EmitDefaultValue = false)]
+        public string Guid { get; set; }
+
+        /// <summary>
+        ///     Id
+        /// </summary>
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public string Id { get; set; }
+
+        /// <summary>
+        ///     Parts
+        /// </summary>
+        [DataMember(Name = "parts", EmitDefaultValue = false)]
+        public OptimizationProfileSaveEntitiesItemPart[] Parts { get; set; }
+    }
+
+    public class OptimizationProfileSaveEntitiesItemPart : GenericParameters
+    {
+        /// <summary>
+        ///     Guid
+        /// </summary>
+        [DataMember(Name = "guid", EmitDefaultValue = false)]
+        public string Guid { get; set; }
+
+        /// <summary>
+        ///     Data
+        /// </summary>
+        [DataMember(Name = "data", EmitDefaultValue = false)]
+        public JObject Data { get; set; }
+
+        /// <summary>
+        ///     Config
+        /// </summary>
+        [DataMember(Name = "config", EmitDefaultValue = false)]
+        public JObject Config { get; set; }
+    }
+}
--- route4me-csharp-sdk/Route4MeSDKLibrary/Managers/OptimizationProfileManagerV5.cs
+++ route4me-csharp-sdk/Route4MeSDKLibrary/Managers/OptimizationProfileManagerV5.cs
@@ -36,12 +36,72 @@ namespace Route4MeSDKLibrary.Managers
         {
             var result = await GetJsonObjectFromAPIAsync<OptimizationProfilesResponse>(new GenericParameters(),
                 R4MEInfrastructureSettingsV5.OptimizationProfilesList,
-                HttpMethodType.Get,
+                HttpMethodType.Post,
                 null,
                 true,
                 false).ConfigureAwait(false);
 
             return new Tuple<OptimizationProfilesResponse, ResultResponse>(result.Item1, result.Item2);
+        }
+
+        /// <summary>
+        /// Saves entities
+        /// </summary>
+        public OptimizationProfileSaveEntities SaveEntities(OptimizationProfileSaveEntities request, out ResultResponse resultResponse)
+        {
+            var response = GetJsonObjectFromAPI<OptimizationProfileSaveEntities>(request,
+                R4MEInfrastructureSettingsV5.OptimizationProfilesSaveEntities,
+                HttpMethodType.Post, null, false, true,
+                out resultResponse, null, true);
+
+            return response;
+        }
+
+        /// <summary>
+        /// Saves entities
+        /// </summary>
+        public async Task<Tuple<OptimizationProfileSaveEntities, ResultResponse>> SaveEntitiesAsync(OptimizationProfileSaveEntities request)
+        {
+            var result = await GetJsonObjectFromAPIAsync<OptimizationProfileSaveEntities>(new GenericParameters(),
+                R4MEInfrastructureSettingsV5.OptimizationProfilesSaveEntities,
+                HttpMethodType.Post,
+                null,
+                true,
+                false,
+                null,
+                true).ConfigureAwait(false);
+
+            return new Tuple<OptimizationProfileSaveEntities, ResultResponse>(result.Item1, result.Item2);
+        }
+
+        /// <summary>
+        /// Deletes entities
+        /// </summary>
+        public OptimizationProfileSaveEntities DeleteEntities(OptimizationProfileDeleteEntitiesRequest request, out ResultResponse resultResponse)
+        {
+            var response = GetJsonObjectFromAPI<OptimizationProfileSaveEntities>(request,
+                R4MEInfrastructureSettingsV5.OptimizationProfilesDeleteEntities,
+                HttpMethodType.Post, null, false, true,
+                out resultResponse, null, true);
+
+            return response;
+        }
+
+        /// <summary>
+        /// Saves entities
+        /// </summary>
+        public async Task<Tuple<OptimizationProfileSaveEntities, ResultResponse>> DeleteEntitiesAsync(OptimizationProfileDeleteEntitiesRequest request)
+        {
+            var result = await GetJsonObjectFromAPIAsync<OptimizationProfileSaveEntities>(new GenericParameters(),
+                R4MEInfrastructureSettingsV5.OptimizationProfilesDeleteEntities,
+                HttpMethodType.Post,
+                null,
+                true,
+                false,
+                null,
+                true).ConfigureAwait(false);
+
+            return new Tuple<OptimizationProfileSaveEntities, ResultResponse>(result.Item1, result.Item2);
         }
     }
 }
