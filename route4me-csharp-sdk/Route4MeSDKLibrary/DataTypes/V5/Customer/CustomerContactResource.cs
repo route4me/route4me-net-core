9501a4532a4503c4868cc3353aa58982f1eb7133
Konstantin Perevertailo <konstantin.perevertailo@gmail.com>
2025-10-17 16:16:56 +0300

    Customers implementation

---
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/Customer/CustomerContactResource.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace Route4MeSDKLibrary.DataTypes.V5.Customers
+{
+    /// <summary>
+    /// Customer contact resource
+    /// </summary>
+    [DataContract]
+    public class CustomerContactResource
+    {
+        /// <summary>
+        /// Contact type
+        /// </summary>
+        [DataMember(Name = "type", EmitDefaultValue = false)]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Contact name
+        /// </summary>
+        [DataMember(Name = "name", EmitDefaultValue = false)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Contact email
+        /// </summary>
+        [DataMember(Name = "email", EmitDefaultValue = false)]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Contact phone
+        /// </summary>
+        [DataMember(Name = "phone", EmitDefaultValue = false)]
+        public string Phone { get; set; }
+    }
+}
---
+++ route4me-csharp-sdk/Route4MeSDKTest/Examples/API5/Customers/CreateCustomerV5.cs
@@ -0,0 +1,27 @@
+﻿using Route4MeSDKLibrary.DataTypes.V5.Customers;
+using System;
+
+namespace Route4MeSDK.Examples
+{
+    public sealed partial class Route4MeExamples
+    {
+        /// <summary>
+        /// The example refers to the process of creating a new customer.
+        /// </summary>
+        public void CreateCustomerV5()
+        {
+            var route4Me = new Route4MeManagerV5(ActualApiKey);
+
+            var request = new StoreRequest
+            {
+                Name = "Test Customer " + DateTime.Now.ToString("yyyyMMddHHmmss"),
+                ExternalId = Guid.NewGuid().ToString("N"),
+                Status = 1
+            };
+
+            var customer = route4Me.CreateCustomer(request, out var resultResponse);
+
+            PrintTestCustomer(customer, resultResponse);
+        }
+    }
+}
