bd9e4bd25910107db56030d0af65344e4320fbf4
yurii-bart <yurii.bartish@bart-solutions.com>
2025-11-12 16:30:18 +0200

    fix formatting

--- route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/Routes/RouteFilterResponse.cs
+++ route4me-csharp-sdk/Route4MeSDKLibrary/DataTypes/V5/Routes/RouteFilterResponse.cs
@@ -247,7 +247,7 @@ namespace Route4MeSDK.DataTypes.V5
         /// </summary>
         [DataMember(Name = "root_member_id", EmitDefaultValue = false)]
         public long? RootMemberId { get; set; }
-        
+
         /// <summary>
         ///     Facility ID's
         /// </summary>
